9e91ceae51a2ea0b05757a5b22f6bffecadd87c7
etime-leo <76693269+etime-leo@users.noreply.github.com>
2020-12-29 11:41:16 +0100

    module 1

---
+++ ssg/site.py
@@ -0,0 +1,20 @@
+import typer
+from pathlib import Path
+
+
+class Site:
+    """A simple example class"""
+
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest + '/' + path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.directory.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob('+'):
+            if path.is_dir():
+                self.create_dir(path)
