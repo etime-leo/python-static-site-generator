691915de944783b728ae639f300626f2f52d5584
etime-leo <76693269+etime-leo@users.noreply.github.com>
2020-12-29 17:22:32 +0100

    module 5

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -46,7 +46,7 @@ class MarkdownParser(Parser):
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
-class ReStructuredText(Parser):
+class ReStructuredTextParser(Parser):
     extensions = [".rst"]
 
     def parse(self, path, source, dest):
